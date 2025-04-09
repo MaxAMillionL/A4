d1ae8f0e48c15ae114731a305a46034c92723151
MaxAMillionL <maxwelllloyd@comcast.net>
2025-04-09 19:29:15 -0400

    Fix errors and nothing

--- 3FT/ft.c
+++ 3FT/ft.c
@@ -580,7 +580,7 @@ int FT_stat(const char *pcPath, boolean *pbIsFile, size_t *pulSize){
    /* Confirm oNCurr is last node of pcPath */
    if(Path_comparePath(Node_getPath(oNCurr), oPPath) != 0){
       Path_free(oPPath);
-      return NO_SUCH_PATH;
+      return NULL;
    }
 
    *pbIsFile = Node_type(oNCurr);
