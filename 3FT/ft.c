198a98ac2855ef927d6f54d01a15de46d948b275
MaxAMillionL <maxwelllloyd@comcast.net>
2025-04-09 19:28:28 -0400

    Fix error message

--- 3FT/ft.c
+++ 3FT/ft.c
@@ -577,16 +577,10 @@ int FT_stat(const char *pcPath, boolean *pbIsFile, size_t *pulSize){
       return MEMORY_ERROR;
    }
 
-   /* Make sure oNCurr is not a directory*/
-   if(Node_type(oNCurr) == FALSE){
-      Path_free(oPPath);
-      return NULL;
-   }
-
    /* Confirm oNCurr is last node of pcPath */
    if(Path_comparePath(Node_getPath(oNCurr), oPPath) != 0){
       Path_free(oPPath);
-      return NULL;
+      return NO_SUCH_PATH;
    }
 
    *pbIsFile = Node_type(oNCurr);
